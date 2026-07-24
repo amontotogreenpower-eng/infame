5f69aa7bb6a8c94621720686286bf33f211fb616
amontotogreenpower-eng <amontoto.greenpower@gmail.com>
2026-07-24 21:57:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const C='infame-v1';
 const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
- 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'];
+ 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js',
+ 'https://unpkg.com/three@0.128.0/build/three.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
